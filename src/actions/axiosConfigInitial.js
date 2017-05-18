b40a50f9cda7562202f0e0b65f08f155da3cf897
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-05-18 14:28:34 -0700

    update api url

--- src/actions/axiosConfigInitial.js
+++ src/actions/axiosConfigInitial.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 const axiosInstance = Axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? '//api.gameswithwords.org/api' : '//localhost/api',
+  baseURL: process.env.NODE_ENV === 'production'
+    ? '/api/whichenglish'
+    : '//localhost/api/whichenglish'
 });
 export default axiosInstance;
-
